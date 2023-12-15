f667b43f14a53bfa359d561c206671067cdf4841
Vibekkarki <58102435+Vibekkarki@users.noreply.github.com>
2023-12-14 16:09:27 -0800

    final commit
    made contact page and minor fixes

--- Assests/js/homepage-script.js
+++ Assests/js/homepage-script.js
@@ -20,3 +20,53 @@ function showSlides() {
 
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+// validate payment page forms
+function validateForm() {
+    var cardNumber = document.getElementById("cardNumber").value;
+    var expiryDate = document.getElementById("expiryDate").value;
+    var cvv = document.getElementById("cvv").value;
+    var cardHolderName = document.getElementById("cardHolderName").value;
+
+    if (!cardNumber || !expiryDate || !cvv || !cardHolderName) {
+        alert("Please fill in all required fields.");
+        return false;
+    }
+
+    alert("Payment successful!");
+
+    window.location.href = "index.html";
+
+    return false; 
+}
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    var valid = true;
+
+    // Name validation
+    var name = document.getElementById('name').value.trim();
+    if (name === '') {
+        valid = false;
+        alert('Please enter your name.');
+    }
+
+    // Email validation
+    var email = document.getElementById('email').value.trim();
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        valid = false;
+        alert('Please enter a valid email address.');
+    }
+
+    // Message validation
+    var message = document.getElementById('message').value.trim();
+    if (message === '') {
+        valid = false;
+        alert('Please enter your message.');
+    }
+
+    if (!valid) {
+        event.preventDefault(); // Prevent form submission if validation fails
+    }
+});
